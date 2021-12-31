f7468d0b5baee4b8f1189c4e880b6f6935cc12b1
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2021-12-31 12:13:14 +0330

    modify class SpringConfig.java rename --> SpringConfig1.java

--- src/main/java/ir/maktab/config/SpringConfig1.java
+++ src/main/java/ir/maktab/config/SpringConfig1.java
@@ -6,17 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SpringConfig {
+public class SpringConfig1 {
 
     @Bean
     public StudentDao studentDao(){
         return new StudentDao();
-    }
-
-    @Bean
-    public StudentService studentService(StudentDao studentDao){
-        StudentService studentService = new StudentService();
-        studentService.setStudentDao(studentDao);
-        return studentService;
     }
 }
