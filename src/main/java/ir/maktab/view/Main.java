77abc83d58b34b1524a4b73717b31547521ee35f
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2021-12-31 15:12:13 +0330

    modify main --> test SpringConfigUser.java

--- src/main/java/ir/maktab/view/Main.java
+++ src/main/java/ir/maktab/view/Main.java
@@ -1,8 +1,23 @@
 package ir.maktab.view;
 
+import ir.maktab.config.SpringConfigUser;
+import ir.maktab.dao.UserDao;
+import ir.maktab.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigUser.class);
 
+        UserDao userDao= (UserDao) context.getBean("userDao");
+        UserService userService1= (UserService) context.getBean("userService");
+        UserService userService2= (UserService) context.getBean("userService");
+        userService1.setName("zahra");
+        userService2.setName("mona");
+        System.out.println("******* spring class get bean");
+        userDao.save();
+        System.out.println(userService1.getName());
+        System.out.println(userService2.getName());
 
     }
 }
