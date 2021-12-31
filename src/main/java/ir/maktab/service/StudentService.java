4b7f3474cccda97719b4de89d5faa46d334086a7
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2021-12-31 11:28:28 +0330

    add setter getter to StudentService

--- src/main/java/ir/maktab/service/StudentService.java
+++ src/main/java/ir/maktab/service/StudentService.java
@@ -1,28 +1,24 @@
 package ir.maktab.service;
 
 import ir.maktab.dao.StudentDao;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class StudentService {
 
-    // old way
-       /* StudentDao studentDao = new StudentDao();
-        studentDao.delete();*/
+    StudentDao studentDao;
 
-    //new way
-    ClassPathXmlApplicationContext
-            context=
-            new ClassPathXmlApplicationContext("applicationContext.xml");
-    // by name
-    StudentDao studentDao = (StudentDao) context.getBean("StudentDao");
-    // by class
-    StudentDao studentDao1 = context.getBean(StudentDao.class);
-
-    public void saveNewStudent(){
+    public void saveNewStudent() {
         studentDao.save();
     }
 
-    public void removeStudent(){
-        studentDao1.delete();
+    public void removeStudent() {
+        studentDao.delete();
+    }
+
+    public StudentDao getStudentDao() {
+        return studentDao;
+    }
+
+    public void setStudentDao(StudentDao studentDao) {
+        this.studentDao = studentDao;
     }
 }
