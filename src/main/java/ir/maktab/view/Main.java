33e6efe34a29b026100949cf859f289aa7668e23
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-02 19:02:26 +0330

    modify class main

--- src/main/java/ir/maktab/view/Main.java
+++ src/main/java/ir/maktab/view/Main.java
@@ -9,15 +9,21 @@ public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigUser.class);
 
-        UserDao userDao= (UserDao) context.getBean("userDao");
-        UserService userService1= (UserService) context.getBean("userService");
-        UserService userService2= (UserService) context.getBean("userService");
-        userService1.setName("zahra");
-        userService2.setName("mona");
+        UserDao userDao = (UserDao) context.getBean("userDao");
+        UserService userService1 = (UserService) context.getBean("userService");
+        UserService userService2 = (UserService) context.getBean("userService");
+        /*userService1.setName("zahra");
+        userService2.setName("mona");*/
         System.out.println("******* spring class get bean");
         userDao.save();
+        userService1.saveUser();
         System.out.println(userService1.getName());
         System.out.println(userService2.getName());
 
+        /*
+        contractor Injection : when beans are related togather
+        setter injection : if it was available will set
+        service injection : its like setter injection
+         */
     }
 }
