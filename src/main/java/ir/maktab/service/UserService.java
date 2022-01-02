1f8b9d41f13e72a82451f75101651ee3519ef256
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-02 19:04:34 +0330

    modify class UserService.java

--- src/main/java/ir/maktab/service/UserService.java
+++ src/main/java/ir/maktab/service/UserService.java
@@ -3,19 +3,24 @@ import ir.maktab.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-
-@Scope("prototype")
+@Lazy //lazy is there less resource ins
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+/*
+@DependsOn("userDao")
+*/
 @Service
+@PropertySource({"classpath:config.properties"})
 public class UserService {
-    @Value("zahra")
+    @Value("${x}")
     String name;
     UserDao userDao;
-
    /* @Autowired
     public UserService(String name,@Qualifier("userDao") UserDao userDao) {
         this.name = name;
